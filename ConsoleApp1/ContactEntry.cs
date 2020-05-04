91d2ee1111a44a12837502125c4507a4749d8629
com.Kondratov <com.Kondratov@gmail.com>
2020-05-04 12:49:35 +0300

    123

---
+++ ConsoleApp1/ContactEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ContactEntry
+    {
+        public ContactEntry()
+        {
+            LastName = "";
+            FirstName = "";
+            PhoneNumber = "";
+            Email = "";
+
+
+
+        }
+
+        public string LastName {get;set;}
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}; Tel.: {2}; Email: {3}",
+                LastName,
+                FirstName,
+                PhoneNumber,
+                Email);
+        }
+    }
+}
